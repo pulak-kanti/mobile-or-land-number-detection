e67dab09c62a89e39f591bc74ec4068ed8668817
pkbhowmick <pkbhowmick007@gmail.com>
2020-11-09 18:02:25 +0600

    add features for any country

--- is_mobile.py
+++ is_mobile.py
@@ -6,11 +6,12 @@ def write_excel_for_mobile(excel_file_path, lower_bound,upper_bound):
     sheet = hcp_excel.sheets['Sheet1']
     i = lower_bound
     while i <= upper_bound:
-        contact_no = sheet.range('C'+str(i)).value
-        if contact_no is None:
+        country = sheet.range('C'+str(i)).value
+        contact_no = sheet.range('D'+str(i)).value
+        if contact_no is None or country in None:
             i+=1
             continue
-        sheet.range('D'+str(i)).value = isMobile("NL",contact_no)
+        sheet.range('D'+str(i)).value = isMobile(country,contact_no)
         i+=1    
     hcp_excel.save()
     hcp_excel.close()
